64c74242e198956ca3b77ccebdb6ca9898eda66f
tvmichael <tvmichael@meta.ua>
2018-05-28 23:54:47 +0300

    temp

---
+++ http/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HTTP</title>
+</head>
+<body>
+
+<h1>HTTP</h1>
+  <p>Test page</p>
+
+  <button id="xbutton" onclick="phonesRequest()">Phones</button>
+  <p>
+    <pre id='phone'><pre>
+  </p>
+
+
+  <script src="script.js"></script>
+</body>
+</html>
